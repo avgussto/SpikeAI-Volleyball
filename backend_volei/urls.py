2d9b5f4681da2999aa89e4ec1135e39a02bc7c64
avgussto <amaralaugustosergio@gmail.com>
2025-03-28 12:03:38 -0300

    começando o site

--- backend_volei/urls.py
+++ backend_volei/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path  # Certifique-se de que esta linha está presente
+from .views import inicio, registro, login# Importando as views que criamos
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', views.register, name='register'),
+    path('', inicio, name='inicio'),  # Tela inicial
+    path('registro/', registro, name='registro'),
+    path('login/', login, name='login') # Tela de registro
 ]
--- backend_volei/views.py
+++ backend_volei/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()  # Salva o novo usuário
-            return redirect('login')  # Redireciona para a página de login ou onde você quiser
-    else:
-        form = RegisterForm()
-    return render(request, 'register.html', {'form': form})
+def registro(request):
+    return render(request, 'registro.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def login(request):
+    return render(request, 'login.html')
